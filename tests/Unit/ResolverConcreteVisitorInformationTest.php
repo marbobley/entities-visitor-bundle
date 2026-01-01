932e1339ec50bfdcd7739d11cf0cf1ccdda012c6
Marbobley <fournier.wilf@gmail.com>
2026-01-01 19:14:27 +0100

    Init test

---
+++ tests/Unit/ResolverConcreteVisitorInformationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Marbobley\EntitiesVisitorBundle\Tests\Unit;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\Mapping\ClassMetadata;
+use Doctrine\Persistence\Mapping\ClassMetadataFactory;
+use Marbobley\EntitiesVisitorBundle\Domain\ServiceImplementation\ResolverConcreteVisitorInformation;
+use Marbobley\EntitiesVisitorBundle\Model\VisitorInformation;
+use PHPUnit\Framework\TestCase;
+
+class ResolverConcreteVisitorInformationTest extends TestCase
+{
+    public function testResolveConcreteVisitorInformationClassReturnsNullIfNoSubclassFound()
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $metadataFactory = $this->createMock(ClassMetadataFactory::class);
+
+        $entityManager->method('getMetadataFactory')->willReturn($metadataFactory);
+        $metadataFactory->method('getAllMetadata')->willReturn([]);
+
+        $resolver = new ResolverConcreteVisitorInformation($entityManager);
+        $result = $resolver->resolveConcreteVisitorInformationClass();
+
+        $this->assertNull($result);
+    }
+
+    public function testResolveConcreteVisitorInformationClassReturnsClassNameIfSubclassFound()
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $metadataFactory = $this->createMock(ClassMetadataFactory::class);
+
+        $metadata = $this->createMock(ClassMetadata::class);
+        $metadata->method('getName')->willReturn(ConcreteVisitorInformation::class);
+
+        $entityManager->method('getMetadataFactory')->willReturn($metadataFactory);
+        $metadataFactory->method('getAllMetadata')->willReturn([$metadata]);
+
+        $resolver = new ResolverConcreteVisitorInformation($entityManager);
+        $result = $resolver->resolveConcreteVisitorInformationClass();
+
+        $this->assertEquals(ConcreteVisitorInformation::class, $result);
+    }
+}
+
+class ConcreteVisitorInformation extends VisitorInformation {}
--- tests/Unit/VisitorListenerTest.php
+++ tests/Unit/VisitorListenerTest.php
@@ -3,11 +3,89 @@
 
 namespace Marbobley\EntitiesVisitorBundle\Tests\Unit;
 
+use Marbobley\EntitiesVisitorBundle\Domain\ServiceImplementation\PersistVisitorInformation;
+use Marbobley\EntitiesVisitorBundle\Domain\ServiceImplementation\ResolverConcreteVisitorInformation;
+use Marbobley\EntitiesVisitorBundle\EventListener\VisitorListener;
+use Marbobley\EntitiesVisitorBundle\Model\VisitorInformation;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
 class VisitorListenerTest extends TestCase
 {
-    public function testCanAccessUnderlyingObject()
+    private $logger;
+    private $persistVisitorInformation;
+    private $resolverConcreteVisitorInformation;
+    private $listener;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->persistVisitorInformation = $this->createMock(PersistVisitorInformation::class);
+        $this->resolverConcreteVisitorInformation = $this->createMock(ResolverConcreteVisitorInformation::class);
+
+        $this->listener = new VisitorListener(
+            $this->logger,
+            $this->persistVisitorInformation,
+            $this->resolverConcreteVisitorInformation
+        );
+    }
+
+    public function testOnTerminateEventWithNoConcreteEntity()
+    {
+        $request = new Request();
+        $event = new TerminateEvent(
+            $this->createMock(HttpKernelInterface::class),
+            $request,
+            null
+        );
+
+        $this->resolverConcreteVisitorInformation->method('resolveConcreteVisitorInformationClass')
+            ->willReturn(null);
+
+        $this->logger->expects($this->once())
+            ->method('warning')
+            ->with($this->stringContains('Aucune entité concrete'));
+
+        $this->persistVisitorInformation->expects($this->never())
+            ->method('save');
+
+        $this->listener->onTerminateEvent($event);
+    }
+
+    public function testOnTerminateEventWithConcreteEntity()
+    {
+        $request = new Request([], [], [
+            '_controller' => 'App\Controller\TestController::index',
+            '_route' => 'test_route'
+        ], [], [], [
+            'REMOTE_ADDR' => '1.2.3.4',
+            'HTTP_USER_AGENT' => 'TestAgent'
+        ]);
+
+        $event = new TerminateEvent(
+            $this->createMock(HttpKernelInterface::class),
+            $request,
+            null
+        );
+
+        $this->resolverConcreteVisitorInformation->method('resolveConcreteVisitorInformationClass')
+            ->willReturn(ConcreteVisitorInformationForListener::class);
+
+        $this->persistVisitorInformation->expects($this->once())
+            ->method('save')
+            ->with($this->callback(function ($visitor) {
+                return $visitor instanceof ConcreteVisitorInformationForListener
+                    && $visitor->ip === '1.2.3.4'
+                    && $visitor->userAgent === 'TestAgent'
+                    && $visitor->controller === 'App\Controller\TestController::index'
+                    && $visitor->route === 'test_route';
+            }));
+
+        $this->listener->onTerminateEvent($event);
     }
 }
+
+class ConcreteVisitorInformationForListener extends VisitorInformation {}
